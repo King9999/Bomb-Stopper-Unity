6e6685de4121cb7619b5295a82efc6aec51362ee
Mike Murray <mikemurray056@gmail.com>
2021-12-22 11:42:44 -0500

    Added UI some elements; giving up on high score table for now
    There doesn't appear to be a way to have a persistent high score table on Itch.io. I haven't completely given up on having one yet.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using TMPro;
 
+//NOTE: Unable to add high score table as there's no reliable way to have persistent data on WebGL without using a server.
 public class GameManager : MonoBehaviour
 {
     public TextAsset hiScoreFile;
 
     public HighScoreTable hiScoreTable;
+    public TextMeshProUGUI scoreGUI;
+    string filePath;                    //contains location of high score table JSON file
+    public TMP_InputField inputField;   //player types words in here
 
     // Start is called before the first frame update
     void Start()
@@ -14,11 +20,34 @@ public class GameManager : MonoBehaviour
         //read from JSON file
         hiScoreTable = JsonUtility.FromJson<HighScoreTable>(hiScoreFile.text);
         Debug.Log("Score: " + hiScoreTable.score);
+        scoreGUI.text = "Score: " + hiScoreTable.score;
+        filePath = Application.dataPath + "/Resources/hiscoretable.json";
+        Debug.Log(filePath);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //When I click the mouse button, score is increased by 100 and is written to file
+        /*if (Input.GetMouseButtonDown(0)) //left button pressed
+        {
+            hiScoreTable.score += 100;
+
+            
+            scoreGUI.text = "Score: " + hiScoreTable.score;
+            //save the result to file
+            string scoreStr = JsonUtility.ToJson(hiScoreTable);
+            Debug.Log("New score data: " + scoreStr);
+            File.WriteAllText(filePath, scoreStr);
+        }*/
     }
+
+    /*public void WriteToFile()
+    {
+        if (!File.Exists(filePath))
+        {
+           // File.Writenew
+           
+        }
+    }*/
 }
