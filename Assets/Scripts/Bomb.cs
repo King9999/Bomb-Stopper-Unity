dd131049b6e9a7d97f318128e0be1bf224061f43
Mike Murray <mikemurray056@gmail.com>
2022-01-26 16:48:15 -0500

    Bomb now flashes when time thresholds are met
    The bomb will flash even faster as time decreases further.

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -6,11 +6,15 @@ using UnityEngine.UI;
 //This class controls the bomb's behaviour, including shrinking the fuse and growing the bomb as the time decreases.
 public class Bomb : MonoBehaviour
 {
-    public GameObject bombBody;
+    public GameObject bombObject;
+    public Image body;
+    public Image redBody;               //bomb turns red when it's about to explode
+    Color redBodyColor;
     public Slider bombFuse;
     float initTime {get;} = 120;       //the usual game time. I use this instead of gameTimer.initTime because 
                                         //gameTimer.initTime can change.
     bool pulseBombCoroutineOn;
+    bool flashBombCoroutineOn;
 
     GameManager gm;
 
@@ -18,6 +22,9 @@ public class Bomb : MonoBehaviour
     void Start()
     {
         bombFuse.value = 1;
+        redBodyColor = redBody.color;
+        redBodyColor.a = 0;
+        redBody.color = new Color(redBodyColor.r, redBodyColor.g, redBodyColor.b, redBodyColor.a);    //transparent by default
         gm = GameManager.instance;  //this is here to ensure the instance isn't null at runtime.
 
         if (gm != null)
@@ -29,12 +36,21 @@ public class Bomb : MonoBehaviour
 
     void Update()
     {
-        if (gm.currentWordCount < gm.totalWordCount)
+        if (!gm.gameTimer.TimeUp() && !gm.gameOver && gm.currentWordCount < gm.totalWordCount)    //only run while stage hasn't cleared
         {
             if (!pulseBombCoroutineOn)
             {
                 pulseBombCoroutineOn = true;
                 StartCoroutine(PulseBomb());
+            }
+
+            if (gm.gameTimer.time / initTime <= 0.5f)
+            {
+                if (!flashBombCoroutineOn)
+                {
+                    flashBombCoroutineOn = true;
+                    StartCoroutine(FlashBomb());
+                }
             }
         }
         else //stage completed
@@ -58,30 +74,66 @@ public class Bomb : MonoBehaviour
     IEnumerator PulseBomb()
     {
         float scaleValue = 0.2f;
-        Vector3 targetScale = new Vector3 (bombBody.transform.localScale.x + scaleValue, bombBody.transform.localScale.y + scaleValue, 1);
-        Vector3 originalScale = bombBody.transform.localScale;
+        Vector3 targetScale = new Vector3 (bombObject.transform.localScale.x + scaleValue, bombObject.transform.localScale.y + scaleValue, 1);
+        Vector3 originalScale = bombObject.transform.localScale;
         float deltaScale;
 
-        while (bombBody.transform.localScale.x < targetScale.x)
+        //expand bomb
+        while (bombObject.transform.localScale.x < targetScale.x)
         {
             deltaScale = Time.deltaTime / 4f;
-            bombBody.transform.localScale = new Vector3(bombBody.transform.localScale.x + deltaScale, 
-                bombBody.transform.localScale.y + deltaScale, 1);
+            bombObject.transform.localScale = new Vector3(bombObject.transform.localScale.x + deltaScale, 
+                bombObject.transform.localScale.y + deltaScale, 1);
             yield return null;
         }
 
         //shrink back to normal
-        while (bombBody.transform.localScale.x > originalScale.x)
+        while (bombObject.transform.localScale.x > originalScale.x)
         {
             deltaScale = Time.deltaTime / 4f;
-            bombBody.transform.localScale = new Vector3(bombBody.transform.localScale.x - deltaScale, 
-                bombBody.transform.localScale.y - deltaScale, 1);
+            bombObject.transform.localScale = new Vector3(bombObject.transform.localScale.x - deltaScale, 
+                bombObject.transform.localScale.y - deltaScale, 1);
             yield return null;
         }
 
         //ensure scale is back to normal.
-        bombBody.transform.localScale = originalScale;
+        bombObject.transform.localScale = originalScale;
         pulseBombCoroutineOn = false;
+
+    }
+
+    //when time is low, bomb will flash red. Flashing increases as time decreases.
+    IEnumerator FlashBomb()
+    {
+        float changeRate = 1.8f;
+
+        //flashing is quicker depending on time
+        if (gm.gameTimer.time / initTime <= 0.1f)
+            changeRate = 3f;
+        else if (gm.gameTimer.time / initTime <= 0.3f)
+            changeRate = 2.4f;
+
+        //turn red
+        while (redBodyColor.a < 1)
+        {
+            redBodyColor.a += changeRate * Time.deltaTime;
+            Debug.Log(redBodyColor.a + " Change Rate: " + changeRate);
+            redBody.color = new Color(redBodyColor.a, redBodyColor.g, redBodyColor.b, redBodyColor.a);
+            yield return null;
+        }
+
+        //go transparent again
+        while (redBodyColor.a > 0)
+        {
+            redBodyColor.a -= changeRate * Time.deltaTime;
+            redBody.color = new Color(redBodyColor.a, redBodyColor.g, redBodyColor.b, redBodyColor.a);
+            yield return null;
+        }
+
+        //reset
+        redBodyColor.a = 0;
+        redBody.color = new Color(redBodyColor.a, redBodyColor.g, redBodyColor.b, redBodyColor.a);
+        flashBombCoroutineOn = false;
 
     }
 }
