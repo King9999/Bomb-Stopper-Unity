e1a03c3170ecfda10fe0ebd4e954e06d3aebbc6b
Mike Murray <mikemurray056@gmail.com>
2022-01-27 10:54:09 -0500

    Added particle system
    Had to add a second camera and have it focus on UI only to get the particles to show up.

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -13,6 +13,10 @@ public class Bomb : MonoBehaviour
     public Slider bombFuse;
     float initTime {get;} = 120;       //the usual game time. I use this instead of gameTimer.initTime because 
                                         //gameTimer.initTime can change.
+
+    public Transform[] sparkPoints;     //these points are where the spark travels along the fuse
+    
+    //coroutine bools
     bool pulseBombCoroutineOn;
     bool flashBombCoroutineOn;
 
