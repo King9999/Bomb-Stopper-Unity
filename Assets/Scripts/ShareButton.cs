9472ca43bfa2a2da1c8689bf5be1f80b2598cb58
Mike Murray <mikemurray056@gmail.com>
2022-02-17 15:29:20 -0500

    Added condition for share button
    Share button will be disabled in WebGL version of game since clipboard isn't accessible.

---
+++ Assets/Scripts/ShareButton.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script's purpose is to not display the share button if the game build is WebGL.
+public class ShareButton : MonoBehaviour
+{
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        #if UNITY_WEBGL
+            gameObject.SetActive(false);
+        #endif
+    }
+
+   
+}
